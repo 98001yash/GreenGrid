62f1f0da3d2753c05be9b42a3c27195df55951ef
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-23 09:45:48 +0530

    initial commit: spring boot setup

---
+++ src/main/java/com/company/GreenGrid/GreenGridApplication.java
@@ -0,0 +1,13 @@
+package com.company.GreenGrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenGridApplication.class, args);
+	}
+
+}
