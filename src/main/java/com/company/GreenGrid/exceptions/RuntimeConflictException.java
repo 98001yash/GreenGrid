0a985b6a5f8b24fda68cf4e21d3871bec0a90dd5
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-23 15:58:54 +0530

    added the resourceNotFoundException

---
+++ src/main/java/com/company/GreenGrid/exceptions/RuntimeConflictException.java
@@ -0,0 +1,7 @@
+package com.company.GreenGrid.exceptions;
+
+public class RuntimeConflictException extends RuntimeException {
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
+}
