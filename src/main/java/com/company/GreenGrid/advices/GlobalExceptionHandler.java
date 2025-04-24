93868fd92f38b693993c77b5ebacbb23364d288f
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-24 08:24:13 +0530

    added the ResourceNotFound controllerAdvice

---
+++ src/main/java/com/company/GreenGrid/advices/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.company.GreenGrid.advices;
+
+
+import com.company.GreenGrid.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourceNotFoundException ex){
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .message(ex.getMessage())
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+}
