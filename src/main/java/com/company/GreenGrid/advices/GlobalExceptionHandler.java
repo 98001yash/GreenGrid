cb73d2c673e6dc94c97b530146ea8033503e3cd1
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-24 08:25:15 +0530

    added the runtimeConflictException controller advice

--- src/main/java/com/company/GreenGrid/advices/GlobalExceptionHandler.java
+++ src/main/java/com/company/GreenGrid/advices/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.company.GreenGrid.advices;
 
 
 import com.company.GreenGrid.exceptions.ResourceNotFoundException;
+import com.company.GreenGrid.exceptions.RuntimeConflictException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +16,15 @@ public class GlobalExceptionHandler {
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(ex.getMessage())
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(RuntimeConflictException.class)
+    public ResponseEntity<ApiResponse<?>> handleRuntimeConflictException(RuntimeConflictException exception) {
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.CONFLICT)
+                .message(exception.getMessage())
                 .build();
         return buildErrorResponseEntity(apiError);
     }
