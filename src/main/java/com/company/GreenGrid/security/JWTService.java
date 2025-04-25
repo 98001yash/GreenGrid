d2f2fe391a5c449cfccabcb12020a3ee6679c1ef
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-25 09:21:55 +0530

    added the getUserIdFromToken method in the JWTService class

--- src/main/java/com/company/GreenGrid/security/JWTService.java
+++ src/main/java/com/company/GreenGrid/security/JWTService.java
@@ -2,6 +2,7 @@ package com.company.GreenGrid.security;
 
 
 import com.company.GreenGrid.entities.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -39,5 +40,14 @@ public class JWTService {
                 .expiration(new Date(System.currentTimeMillis()  + 1000L *60*60*24*30*6))
                 .signWith(getSecretKey())
                 .compact();
+    }
+
+    public Long getUserIdFromToken(String token){
+        Claims claims = Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return Long.valueOf(claims.getSubject());
     }
 }
