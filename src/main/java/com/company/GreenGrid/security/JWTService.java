10f9b737d4b6918bc7df80c080656260c4cb527a
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-25 09:19:44 +0530

    added the generateAccessToken method in the JWTService

--- src/main/java/com/company/GreenGrid/security/JWTService.java
+++ src/main/java/com/company/GreenGrid/security/JWTService.java
@@ -1,12 +1,15 @@
 package com.company.GreenGrid.security;
 
 
+import com.company.GreenGrid.entities.User;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Service
 public class JWTService {
@@ -16,5 +19,16 @@ public class JWTService {
 
     public SecretKey getSecretKey(){
         return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(User user){
+        return Jwts.builder()
+                .subject(user.getId().toString())
+                .claim("email", user.getEmail())
+                .claim("roles", user.getRoles().toString())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000*60*100))
+                .signWith(getSecretKey())
+                .compact();
     }
 }
