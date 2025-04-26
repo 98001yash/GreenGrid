d2e111b011ac2205d407eb2f3e87512496c82b03
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-26 09:33:06 +0530

    added the refresh controller

--- src/main/java/com/company/GreenGrid/controller/AuthController.java
+++ src/main/java/com/company/GreenGrid/controller/AuthController.java
@@ -12,10 +12,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/auth")
@@ -39,5 +42,20 @@ public class AuthController {
 
         httpServletResponse.addCookie(cookie);
         return ResponseEntity.ok(new LoginResponseDto(tokens[0]));
+    }
+
+
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> refresh(HttpServletRequest request) {
+        String refreshToken = Arrays.stream(request.getCookies()).
+                filter(cookie -> "refreshToken".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new AuthenticationServiceException("Refresh token not found inside the Cookies"));
+
+        String accessToken = authService.refreshToken(refreshToken);
+
+        return ResponseEntity.ok(new LoginResponseDto(accessToken));
     }
 }
