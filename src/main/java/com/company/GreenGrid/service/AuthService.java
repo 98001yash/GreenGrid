2de2e83c4412cff8ef4e5acef93daa158c609175
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-26 09:04:46 +0530

    added the login Service

---
+++ src/main/java/com/company/GreenGrid/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.company.GreenGrid.service;
+
+
+import com.company.GreenGrid.entities.User;
+import com.company.GreenGrid.repository.UserRepository;
+import com.company.GreenGrid.security.JWTService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTService jwtService;
+
+    public String[] login(String email,String password){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email,password)
+        );
+        User user = (User) authentication.getPrincipal();
+        String accessToken = jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        return new String[]{accessToken, refreshToken};
+
+    }
+}
