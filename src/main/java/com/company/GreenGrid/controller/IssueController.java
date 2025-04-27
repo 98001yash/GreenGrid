e882bdbc7a73a137cc686b14edc014ca4a164906
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-27 17:00:25 +0530

    created the controller to reportIssue

---
+++ src/main/java/com/company/GreenGrid/controller/IssueController.java
@@ -0,0 +1,25 @@
+package com.company.GreenGrid.controller;
+
+
+import com.company.GreenGrid.dtos.IssueRequestDto;
+import com.company.GreenGrid.dtos.IssueResponseDto;
+import com.company.GreenGrid.service.IssueService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/issues")
+@RequiredArgsConstructor
+@Slf4j
+public class IssueController {
+
+    private final IssueService issueService;
+
+    @PostMapping
+    public IssueResponseDto reportIssue(@RequestBody IssueRequestDto issueRequestDto,
+                                        @RequestParam Long userId){
+        log.info("Api called: Report issue by userId: {}",userId);
+        return issueService.reportIssue(issueRequestDto, userId);
+    }
+}
